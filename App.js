39a14e0b42cb3b8e7290124b17ba15dca3108c97
tahasama <taha.maatof@gmail.com>
2021-11-25 15:56:39 +0100

    app design

--- App.js
+++ App.js
@@ -1,21 +1,148 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import React from "react";
+import { StyleSheet, Text, View, Platform, Image } from "react-native";
+import { AntDesign } from "@expo/vector-icons";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <>
+      <View style={styles.infos}>
+        <View style={styles.upperInfos}>
+          <Text style={styles.repoName}>repository name</Text>
+          <Text style={styles.repoDesc}>repository description</Text>
+        </View>
+        <View style={styles.bottomInfos}>
+          <Image
+            source={{
+              uri: "https://e7.pngegg.com/pngimages/84/165/png-clipart-united-states-avatar-organization-information-user-avatar-service-computer-wallpaper.png",
+            }}
+            style={styles.tinyLogo}
+          />
+          <Text style={styles.owner}>repository owner</Text>
+
+          <View style={styles.stars}>
+            <AntDesign
+              name="star"
+              size={16}
+              color="black"
+              style={styles.star}
+            />
+            <Text style={styles.nb}>100</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.infos}>
+        <View style={styles.upperInfos}>
+          <Text style={styles.repoName}>repository name</Text>
+          <Text style={styles.repoDesc}>
+            repository description pository descriptionpository
+            descriptionpository description
+          </Text>
+        </View>
+        <View style={styles.bottomInfos}>
+          <Image
+            source={{
+              uri: "https://e7.pngegg.com/pngimages/84/165/png-clipart-united-states-avatar-organization-information-user-avatar-service-computer-wallpaper.png",
+            }}
+            style={styles.tinyLogo}
+          />
+          <Text style={styles.owner}>repository owner</Text>
+
+          <View style={styles.stars}>
+            <AntDesign
+              name="star"
+              size={16}
+              color="black"
+              style={styles.star}
+            />
+            <Text style={styles.nb}>100</Text>
+          </View>
+        </View>
+      </View>
+      <View style={styles.infos}>
+        <View style={styles.upperInfos}>
+          <Text style={styles.repoName}>repository name</Text>
+          <Text style={styles.repoDesc}>
+            repository description pository descriptionpository
+            descriptionpository descriptionpository descriptionpository
+            descriptionpository descriptionpository description
+          </Text>
+        </View>
+        <View style={styles.bottomInfos}>
+          <Image
+            source={{
+              uri: "https://e7.pngegg.com/pngimages/84/165/png-clipart-united-states-avatar-organization-information-user-avatar-service-computer-wallpaper.png",
+            }}
+            style={styles.tinyLogo}
+          />
+          <Text style={styles.owner}>repository owner</Text>
+
+          <View style={styles.stars}>
+            <AntDesign
+              name="star"
+              size={16}
+              color="black"
+              style={styles.star}
+            />
+            <Text style={styles.nb}>100</Text>
+          </View>
+        </View>
+      </View>
+    </>
   );
 }
 
 const styles = StyleSheet.create({
-  container: {
+  settings: {
+    justifyContent: "center",
+    alignItems: "center",
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  bottomInfos: {
+    margin: 20,
+    display: "flex",
+    flexDirection: "row",
+  },
+  avatar: {
+    flex: 1,
+    paddingBottom: 20,
+  },
+  owner: {
+    flex: 1,
+    fontStyle: "italic",
+    marginLeft: 10,
+    marginTop: 6,
+  },
+  nb: { marginTop: 4 },
+  star: { marginRight: 5, marginTop: 5 },
+  stars: {
+    display: "flex",
+    flexDirection: "row",
+    marginTop: 5,
+    marginLeft: 100,
+  },
+  upperInfos: {
+    marginLeft: 20,
+  },
+  repoName: {
+    fontSize: 24,
+    marginBottom: 5,
+  },
+  repoDesc: {
+    fontWeight: "300",
+  },
+  loaderStyle: {
+    marginVertical: 16,
+    alignItems: "center",
+  },
+  infos: { marginBottom: 30 },
+  screen: {
+    // paddingTop: Constants.statusBarHeight,
+    paddingTop: Platform.OS === "android" ? 40 : 20,
+  },
+
+  tinyLogo: {
+    width: 30,
+    height: 30,
   },
 });
